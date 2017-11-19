a40865451133f0c89dc0f0e4b6d5bf880b614bfe
Rattanak Chea <rattanak.chea@gmail.com>
2017-11-18 20:09:36 -0800

    fixed typo, remove comments

--- src/app/project-item-layout-2/project-item-layout-2.component.ts
+++ src/app/project-item-layout-2/project-item-layout-2.component.ts
@@ -1,17 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { StoreService} from '../services/store.service';
 
 @Component({
   selector: 'project-item-layout-2',
   templateUrl: './project-item-layout-2.component.html',
   styleUrls: ['./project-item-layout-2.component.sass']
 })
-export class ProjectItemLayout2Component implements OnInit {
+export class ProjectItemLayout2Component {
   @Input() item: any;
-  
-  constructor(private storeService: StoreService) { }
 
-  ngOnInit() {
-  }
-
+  constructor() { }
 }
